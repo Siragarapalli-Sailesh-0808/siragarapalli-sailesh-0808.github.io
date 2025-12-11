e6b7147b0358c64b9fd3d9e715bba5a570aff9cd
Siragarapalli-Sailesh-0808 <sailesh@example.com>
2025-12-11 09:45:22 +0530

    Simplify routing for root domain: Remove SPA script and basename

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <ThemeProvider>
-      <Router basename="/">
+      <Router>
         <div className="app">
           {/* Animated background blobs */}
           <div className="animated-bg">
